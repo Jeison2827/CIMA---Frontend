4d24f328638d563d36deff13ccffe05b19b3c586
jeison2827 <jeisonps28@gmail.com>
2024-10-15 01:41:18 -0500

    Creación create users

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import ClientTable from './components/Client/ClientTable';
 import RoleManagement from './components/Roles/RoleManagement'; // Nueva gestión de roles
 import ProtectedRoute from './routes/ProtectedRoute';
 import Dashboard from './components/Dashboard/Dashboard'; // Importa el Dashboard aquí
-
+import CreateUser from './components/Client/CreateUser';
 const App = () => {
   const { user } = useSelector((state) => state.auth);
 
@@ -29,12 +29,14 @@ const App = () => {
           }
         />
 
+
         {/* Ruta protegida para crear un cliente */}
         <Route
-          path="/create-client"
+          path="/create-users-register"
           element={
             <ProtectedRoute roles={['Admin']}>
-              <CreateClient />
+              {/*<CreateClient />*/}
+              <CreateUser />
             </ProtectedRoute>
           }
         />
